c5e0833a60d4973cd152b26e750356cdd75eac8f
Your Name <13726261186@163.com>
2020-03-20 15:54:49 +0800

    springboot2

--- springboot01/src/test/java/com/ricky/example/Springboot01ApplicationTests.java
+++ springboot01/src/test/java/com/ricky/example/Springboot01ApplicationTests.java
@@ -8,10 +8,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class Springboot01ApplicationTests {
+
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -22,6 +28,18 @@ public class Springboot01ApplicationTests {
 				.andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
 		int status = mvcResult.getResponse().getStatus();
 		System.out.println(status);*/
+	}
+
+
+	@Test
+	public void testMethod_1(){
+		Map<String,Object> map = new HashMap<>();
+		List<String> list = Arrays.asList("a","b");
+		map.put("A","A-1");
+		map.put("B",22);
+		map.put("C",list);
+		System.out.println(map.toString());
+
 	}
 
 
