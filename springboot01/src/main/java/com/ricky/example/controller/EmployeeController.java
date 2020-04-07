9f9a0f0b4606543a0d5d369b1878a3b618a99090
Your Name <13726261186@163.com>
2020-04-07 16:07:16 +0800

    springboot2

--- springboot01/src/main/java/com/ricky/example/controller/EmployeeController.java
+++ springboot01/src/main/java/com/ricky/example/controller/EmployeeController.java
@@ -1,16 +1,20 @@
 package com.ricky.example.controller;
 
 import com.ricky.example.entity.Employee;
+import com.ricky.example.rest.ListResult;
+import com.ricky.example.rest.Result;
 import com.ricky.example.rest.SingleResult;
 import com.ricky.example.service.EmployeeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author ricky
@@ -34,5 +38,36 @@ public class EmployeeController {
         long end = new Date().getTime();
         System.out.println("请求时间:"+(end-start));
         return new SingleResult(true,"操作成功",employee);
+    }
+
+    @ApiOperation(value = "/update",notes = "更新employee对象")
+    @PostMapping(value = "/update")
+    public SingleResult update(Employee employee){
+        long start = new Date().getTime();
+        int flag = employeeService.update(employee);
+        long end = new Date().getTime();
+        if(flag>0){
+            return new SingleResult(true,"更新操作成功");
+        }else {
+            return new SingleResult(false,"更新操作失败");
+        }
+    }
+
+    @ApiOperation(value = "/delete",notes = "删除employee对象")
+    @PostMapping(value = "/delete")
+    public Result delete(Integer id) {
+        employeeService.delete(id);
+        return new Result(true,"删除操作成功");
+    }
+
+    @ApiOperation(value = "/queryAll",notes = "查询所有employee对象")
+    @GetMapping(value = "/queryAll")
+    public ListResult queryAll(){
+        long start = new Date().getTime();
+        List<Employee> employeeList = employeeService.queryAll();
+        long end = new Date().getTime();
+
+        System.out.println("用时 ："+(end-start));
+        return new ListResult(true,"查询所有操作成功",employeeList);
     }
 }
--- springboot01/src/main/java/com/ricky/example/service/Imp/EmployeeServiceImp.java
+++ springboot01/src/main/java/com/ricky/example/service/Imp/EmployeeServiceImp.java
@@ -5,6 +5,8 @@ import com.ricky.example.entity.Employee;
 import com.ricky.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -27,22 +29,71 @@ public class EmployeeServiceImp implements EmployeeService {
         return employeeDao.insert(employee);
     }
 
+
+
+    /**
+     * @CacheEvict 缓存清除,E:删除员工后就不需要保留相关的缓存数据
+     *   1.key = "#id"：删除指定的id
+     *   2.allEntries = true：清除这个缓存所有的数据
+     *   3.beforeInvocation
+     *     beforeInvocation = false:
+     *       指定缓存清除在方法执行之前执行,默认在方法之后执行;如果出现异常则不会清除缓存
+     *     beforeInvocation = true:
+     *       代表缓存清除操作是在方法运行之前执行，无论方法出现异常，缓存都会清除
+     */
+    @CacheEvict(value = "emp")
     @Override
     public int delete(Integer id) {
         return employeeDao.delete(id);
     }
 
+
+
+    /**
+     * @CachePut 既调用方法，又更新缓存数据；
+     *   通过设定key来更新数据：
+     *     key = "#employee.id"
+     *     key = "#result.id"
+     */
+
+    @CachePut(value = "emp",key = "#result.id")
     @Override
     public int update(Employee employee) {
         return employeeDao.update(employee);
     }
 
+
+
+
+
+    /**
+     * 将方法的运行结果进行缓存：以后读取相同的数据直接从缓存读取，不调用方法 @Cacheable
+     *
+     * CacheManager管理多个Cache组件，对缓存的真正CRUD组件中，每一个缓存都有自己唯一的名字
+     * 属性：
+     *    cacheNames/value：指定缓存组件的名字
+     *
+     *  key和keyGenerator二选一
+     *    key：缓存数据使用的KEY，可以用他指定数据，默认是使用方法参数的值，1-方法的返回值
+     *         值用SpEl表达式指定：#id：参数id的值， #a0 #p0 #root.args[0]
+     *       E:key = "#root.methodName+'['+#id+']'"
+     *    keyGenerator：key的生成器，可以自己指定key的生成器的组件id
+     *       E:keyGenerator = "keyGenerator";指定自定义key生成策略注册Bean的ID名称
+     *
+     *    cacheManager：指定缓存管理器；或者cacheResolver指定获取解析器
+     *    condition：指定符合条件下才缓存
+     *       E:condition = "#id>0"
+     *    unless：否定缓存；当unless指定的条件为true时，方法的返回值就不会被缓存，可以获取结果进行判断
+     *       E:unless = "#result == null"
+     *    sync：是否采用异步模式
+     */
     @Cacheable(cacheNames = {"emp"})
     @Override
     public Employee findById(Integer id) {
         return employeeDao.findById(id);
     }
 
+    @Cacheable(cacheNames = {"emp"})
     @Override
     public List<Employee> queryAll() {
         return employeeDao.queryAll();
