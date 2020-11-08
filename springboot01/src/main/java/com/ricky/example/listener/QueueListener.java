4b495766e999a1132fd7bc270f74d626f21feb5f
Your Name <13726261186@163.com>
2020-11-08 12:30:41 +0800

    springboot2

--- springboot01/src/main/java/com/ricky/example/listener/QueueListener.java
+++ springboot01/src/main/java/com/ricky/example/listener/QueueListener.java
@@ -1,5 +1,7 @@
 package com.ricky.example.listener;
 
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,8 +10,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class QueueListener {
-    //@JmsListener(destination = "common.queue", containerFactory = "jmsListenerContainerQueue")
-    //@SendTo("out.queue")//SendTo的意思是把返回结果推送到out.queue队列
+    @JmsListener(destination = "common.queue", containerFactory = "jmsListenerContainerQueue")
+    @SendTo("out.queue")//SendTo的意思是把返回结果推送到out.queue队列
     public void receive(String text){
         System.out.println("QueueListener: consumer-a 收到一条信息: " + text);
        // return "consumer-a received : " + text;
