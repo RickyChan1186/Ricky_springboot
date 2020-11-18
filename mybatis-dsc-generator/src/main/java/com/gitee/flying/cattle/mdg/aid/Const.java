61bc1b58a601afdecfb5cec766e20343fc8b011c
Your Name <13726261186@163.com>
2020-11-18 22:58:27 +0800

    springboot2

--- mybatis-dsc-generator/src/main/java/com/gitee/flying/cattle/mdg/aid/Const.java
+++ mybatis-dsc-generator/src/main/java/com/gitee/flying/cattle/mdg/aid/Const.java
@@ -14,6 +14,7 @@ package com.gitee.flying.cattle.mdg.aid;
  * @author: flying.cattle
  */
 public class Const {
+
 	/********************** 响应 ****************************/
 	public static final String CODE_SUCCESS = "200"; // 成功
 	public static final String CODE_FAILED = "0"; // 失败
